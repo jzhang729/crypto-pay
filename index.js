8b8c000591ffccb9d8e4b82d2839f66af02fa640
Jordan Zhang <jordanzhang@gmail.com>
2018-01-15 16:44:10 -0800

    Removing all files in .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const shopifyApi = require('shopify-api-node');
+const http = require('http');
+const pug = require('pug');
+
 const app = express();
 
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send({ goodbye: 'there' });
+  res.send({ Hello: 'World' });
+});
+
+app.get('/:id', (req, res) => {
+  res.render('pay');
 });
 
 const PORT = process.env.port || 5000;
