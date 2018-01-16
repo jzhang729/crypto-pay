7bb886e64c568018833c017c52adc1352cd94b62
Jordan Zhang <jordanzhang@gmail.com>
2018-01-15 16:46:28 -0800

    PORT correction.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/:id', (req, res) => {
   res.render('pay');
 });
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
