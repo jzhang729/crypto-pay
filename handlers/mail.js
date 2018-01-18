842eb6ca38601723fe86bd797e76c88eeb159340
Jordan Zhang <jordanzhang@gmail.com>
2018-01-18 15:34:12 -0800

    Try to resolve path instead on production.

--- handlers/mail.js
+++ handlers/mail.js
@@ -9,9 +9,9 @@ const transport = require('mailgun-js')({
 exports.send = ({ fields }) => {
   transport.messages().send({
     from: 'Crypto Pay <info@premiumsound.com>',
-    to: 'jordan@headphones.com, taron@headphones.com',
+    to: process.env.MG_MAIL_RECIPIENTS,
     subject: 'Customer wants to pay with Crypto!',
-    text: `${name} at ${email} is interested in buying the ${product} with ${currency}. Please calculate the cost of shipping and then send them the wallet address and total amount due.`
+    text: `${name} at ${email} is interested in buying the ${product} with ${currency}. Please calculate the cost of shipping and then send them the wallet address of ${process.env.RAIBLOCKS_WALLET} and total amount due.`
   }, (error, body) => {
     if (error) { return console.log(error); }
     console.log(body);
--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'apps', 'crypto-pay', 'public')));
+} else {
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
