666c6a7d4b8d6f75e0662674f925d9efa6560573
Jordan Zhang <jordanzhang@gmail.com>
2018-01-17 16:24:09 -0800

    Changed outer div to see if embedded styles apply.

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
